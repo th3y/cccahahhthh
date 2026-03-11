275b63c2c393536fae86c2cb023e51461f2000e7
JNC <th3y@hotmail.com>
2026-03-11 00:53:27 -0500

    dddd

--- server.js
+++ server.js
@@ -13,32 +13,45 @@ const rooms = {}
 
 io.on("connection",(socket)=>{
 
-socket.on("join-room",(room)=>{
+socket.on("join-room",(data)=>{
+
+const room=data.room
+const nick=data.nick
 
 socket.join(room)
-socket.room = room
+
+socket.room=room
+socket.nick=nick
 
 if(!rooms[room]){
-rooms[room] = {
+
+rooms[room]={
 history:[],
-users:[]
-}
+users:{}
 }
 
-rooms[room].users.push(socket.id)
+}
+
+rooms[room].users[socket.id]=nick
 
 /* enviar historial */
 
 socket.emit("history",rooms[room].history)
 
+/* avisar que alguien entró */
+
+socket.to(room).emit("user-joined",nick)
+
 })
+
+/* MENSAJE */
 
 socket.on("message",(data)=>{
 
-const room = data.room
+const room=data.room
 if(!rooms[room]) return
 
-const msg = {
+const msg={
 nick:data.nick,
 text:data.text,
 time:data.time
@@ -46,42 +59,56 @@ time:data.time
 
 rooms[room].history.push(msg)
 
-/* limite historial */
+/* limitar historial */
 
-if(rooms[room].history.length > 50){
+if(rooms[room].history.length>50){
+
 rooms[room].history.shift()
+
 }
 
 io.to(room).emit("message",msg)
 
 })
 
+/* BORRAR CHAT */
+
 socket.on("delete-chat",(data)=>{
 
 const room=data.room
 const code=data.code
 
-if(code !== "delete123") return
+if(code!=="delete123") return
 
 if(rooms[room]){
+
 rooms[room].history=[]
+
 }
 
 io.to(room).emit("chat-deleted")
 
 })
 
+/* DESCONECTAR */
+
 socket.on("disconnect",()=>{
 
-const room = socket.room
+const room=socket.room
+const nick=socket.nick
+
 if(!room || !rooms[room]) return
 
-rooms[room].users = rooms[room].users.filter(u=>u!==socket.id)
+delete rooms[room].users[socket.id]
+
+socket.to(room).emit("user-left",nick)
 
 })
 
 })
 
 server.listen(3000,()=>{
+
 console.log("server running")
+
 })
