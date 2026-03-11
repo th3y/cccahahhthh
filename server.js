1aad462d250bbff0efb87812ccf331a80c058c94
JNC <th3y@hotmail.com>
2026-03-11 00:38:16 -0500

    asd

--- server.js
+++ server.js
@@ -6,41 +6,138 @@ const app = express()
 const server = http.createServer(app)
 
 const io = new Server(server,{
-  cors:{
-    origin:"*"
-  }
+cors:{ origin:"*" }
 })
 
 app.use(express.static("public"))
 
-let rooms={}
+let rooms = {}
+
+/*
+rooms = {
+
+  hash_room:{
+     users:[
+        {id:socketid}
+     ],
+     history:[
+        {nick:"juan",text:"hola",time:"..."}
+     ]
+  }
+
+}
+*/
 
 io.on("connection",(socket)=>{
 
-  socket.on("join-room",(room)=>{
+/* JOIN ROOM */
 
-    socket.join(room)
+socket.on("join-room",(roomHash)=>{
 
-    if(!rooms[room]) rooms[room]=[]
+if(!rooms[roomHash]){
 
-    rooms[room].push(socket.id)
+rooms[roomHash]={
+users:[],
+history:[]
+}
 
-  })
+}
 
-  socket.on("message",(data)=>{
+let users = rooms[roomHash].users
 
-    io.to(data.room).emit("message",data)
+/* limitar a 2 usuarios */
 
-  })
 
-  socket.on("delete-chat",(data)=>{
+users.push({
+id:socket.id
+})
 
-    io.to(data.room).emit("clear")
+socket.join(roomHash)
 
-  })
+/* enviar historial */
+
+socket.emit("history",rooms[roomHash].history)
 
 })
 
+/* MENSAJE */
+
+socket.on("message",(data)=>{
+
+let room = data.room
+
+if(!rooms[room]) return
+
+rooms[room].history.push({
+nick:data.nick,
+text:data.text,
+time:data.time
+})
+
+/* limitar historial tamaño */
+
+if(rooms[room].history.length > 100){
+rooms[room].history.shift()
+}
+
+io.to(room).emit("message",data)
+
+})
+
+/* BORRAR CHAT */
+
+socket.on("delete-chat",(data)=>{
+
+let room = data.room
+
+if(!rooms[room]) return
+
+rooms[room].history = []
+
+io.to(room).emit("clear")
+
+})
+
+/* DISCONNECT */
+
+socket.on("disconnect",()=>{
+
+for(const room in rooms){
+
+rooms[room].users =
+rooms[room].users.filter(u=>u.id !== socket.id)
+
+if(rooms[room].users.length === 0){
+delete rooms[room]
+}
+
+}
+
+})
+
+})
+
+/* LIMPIAR MENSAJES > 48 HORAS */
+
+setInterval(()=>{
+
+const now = Date.now()
+const limit = 1000 * 60 * 60 * 48
+
+for(const room in rooms){
+
+rooms[room].history = rooms[room].history.filter(msg=>{
+
+const msgTime = new Date(msg.time).getTime()
+
+return (now - msgTime) < limit
+
+})
+
+}
+
+}, 60 * 60 * 1000) // cada 1 hora
+
 server.listen(3000,()=>{
-  console.log("server running")
+console.log("server running")
 })
