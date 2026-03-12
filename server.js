3a34330933c8406781ea66d50cc6c3c906916735
JNC <th3y@hotmail.com>
2026-03-12 01:08:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -150,6 +150,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("Server running on port", process.env.PORT || 3000);
+server.listen(process.env.PORT || 8080, () => {
+  console.log("Server running on port", process.env.PORT || 8080);
 });
